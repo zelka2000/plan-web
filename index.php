c578cbc1104a9262d68cbef01e2aa6eb1fadbb4a
Anton Zelenov <AntonZelenov8282@yandex.ru>
2024-12-08 23:02:29 +0300

    add index.php

--- index.php
+++ index.php
@@ -163,8 +163,6 @@ $db = new Database();
     <?php endforeach; ?>
 </div>
 
-
-<!-- Подключаем GSAP и ScrollTrigger через CDN -->
 <!-- Подключаем GSAP и ScrollTrigger через CDN -->
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.2/gsap.min.js"></script>
