0121a8edb586b9731b783b6d7838c2870d6c74b3
Anton Zelenov <AntonZelenov8282@yandex.ru>
2024-12-08 21:44:51 +0300

    Initial commit

---
+++ components/header.php
@@ -0,0 +1,16 @@
+<header class="header" id="menu">
+    <div class="logo">
+        <img src="../img/logo.svg" alt="plan-web" style="width: 42px; height: 42px;">
+        <h1 class="logo-text">PLAN WEB</h1>
+    </div>
+    <div class="navigator">
+        <a href="" class="nav-link" style="margin-left: 0px;">Услуги</a>
+        <a href="" class="nav-link">Цены</a>
+        <a href="" class="nav-link">Работы</a>
+        <a href="" class="nav-link">О нас</a>
+    </div>
+    <div class="button">
+        <button class="lead">Оставить заявку</button>
+        <button class="menu">···</button>
+    </div>
+</header>
